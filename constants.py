4dba455eb5c1ddb921d8c1453f23eec5645a4253
Isaiah Chiraira <itchiraira.com>
2023-08-04 22:15:44 +0200

    Here we go

---
+++ constants.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = "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"
